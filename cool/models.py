804ff70aab5be421f1063f8dff77c7c7d29dc3d7
HardikoAgarwal <hardikagg1874@gmail.com>
2024-07-24 20:02:48 +0530

    Added some Models

--- cool/models.py
+++ cool/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    date_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return {self.name}
--- cool/views.py
+++ cool/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 def base(request):
     str = 'This is a string'
     number = 16
+    students = Student.objects.all()
     data = {
         'string' : str,
         'number' : number,
+        'students' : students
          
     }
     return render(request,'cool/base.html', data)
